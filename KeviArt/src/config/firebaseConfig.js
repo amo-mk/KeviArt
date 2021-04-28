4770fd9e7e0819ed1e4e8d14cc80994ff1ab1495
Amos Mutai <33829355+amo-mk@users.noreply.github.com>
2021-04-28 09:49:31 +0300

    Changed API keys for privacy purpose

--- KeviArt/src/config/firebaseConfig.js
+++ KeviArt/src/config/firebaseConfig.js
@@ -7,12 +7,12 @@ import 'firebase/auth'
 // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyAMiiPqn6eoacppVQdS-qjOewc0zDKxMrc",
+    apiKey: "AIzaSyAMiiPqn6e", //changed
     authDomain: "keviart-993df.firebaseapp.com",
     projectId: "keviart-993df",
-    storageBucket: "keviart-993df.appspot.com",
+    storageBucket: "keviart.appspot.com",//changed
     messagingSenderId: "762866805383",
-    appId: "1:762866805383:web:35ee005a35f3e637546622",
+    appId: "1:7",//changed
     measurementId: "G-SM01NE7KSM"
   };
   // Initialize Firebase
